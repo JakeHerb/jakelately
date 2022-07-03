7ff6325dd65589ad961c6e8a97b0197743161c37
Jake <mynonspamemailaddress@gmail.com>
2022-07-02 17:45:18 -0700

    [Web3] Add Metamask connection, enabling Metamask Login and account detection.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import contract from './contracts/NFTCollectible.json';
+
+const contractAddress = "0x355638a4eCcb777794257f22f50c289d4189F245";
+const abi = contract.abi;
 
 function App() {
+
+    const [currentAccount, setCurrentAccount] = useState(null);
+
+  const checkWalletIsConnected = () => { 
+    const { ethereum } = window;
+
+    if (!ethereum) {
+        console.log("Ensure you have Metamask installed in your browser.");
+    } else {
+        console.log("Wallet detected: Metamask connection available.");
+    }
+  }
+
+  const connectWalletHandler = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+        alert("Please Install Metamask!");
+    }
+
+    try {
+        const ethAccounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        console.log("Ethereum account detected. ---> Primary Address: ", ethAccounts[0]);
+        setCurrentAccount(ethAccounts[0]);
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+  const mintNftHandler = () => { }
+
+  const connectWalletButton = () => {
+    return (
+      <button onClick={connectWalletHandler} className='cta-button connect-wallet-button'>
+        Connect Wallet
+      </button>
+    )
+  }
+
+  const mintNftButton = () => {
+    return (
+      <button onClick={mintNftHandler} className='cta-button mint-nft-button'>
+        Mint NFT
+      </button>
+    )
+  }
+
+  useEffect(() => {
+    checkWalletIsConnected();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and touch yourself.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-app'>
+      <h1>Scrappy Squirrels Tutorial</h1>
+      <div>
+        {connectWalletButton()}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
