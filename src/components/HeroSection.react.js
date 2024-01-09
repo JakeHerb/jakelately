178f774cb64ce19d88c5be8d2adc277c00c68f77
Jake Herbel <iamjake@MacBook-Pro.local>
2024-01-08 23:14:18 -0800

    Dan

--- src/components/HeroSection.react.js
+++ src/components/HeroSection.react.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './css/HeroSection.css'; // Assuming you have a separate CSS file
-import heroImage from '../media/contrastHero.png';
+import heroImage from '../media/ass.png';
 
 
 const HeroSection = ({ bannerText, aboutTitle, aboutText }) => {
@@ -8,7 +8,9 @@ const HeroSection = ({ bannerText, aboutTitle, aboutText }) => {
       <section className="hero-section" style={{ backgroundImage: `url(${heroImage})` }}>
         <div className="hero-content">
           <div className="image-container">
-            <img src="https://media.licdn.com/dms/image/D5603AQEQ7HYqf3Wd3w/profile-displayphoto-shrink_800_800/0/1699404702054?e=2147483647&v=beta&t=ToaJSX_iv2_OYNRh3STOB1VZ7_3z42cMA1NXPtUhuF0" alt="Profile" />
+            {/* <img src="https://media.licdn.com/dms/image/D5603AQEQ7HYqf3Wd3w/profile-displayphoto-shrink_800_800/0/1699404702054?e=2147483647&v=beta&t=ToaJSX_iv2_OYNRh3STOB1VZ7_3z42cMA1NXPtUhuF0" alt="Profile" /> */}
+            <img src={heroImage} alt="Profile" />
+            
           </div>
           <div className="text-container">
             <h2>{aboutTitle}</h2>
