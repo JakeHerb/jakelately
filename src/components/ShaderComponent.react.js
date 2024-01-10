35f063722ba3c42856df8e67174bc80ea844986f
Jake Herbel <iamjake@MacBook-Pro.local>
2024-01-09 23:56:31 -0800

    Made a star thingy

--- src/components/ShaderComponent.react.js
+++ src/components/ShaderComponent.react.js
@@ -222,46 +222,24 @@ const ShaderComponent = () => {
 
     uniform vec2 u_resolution; // Equivalent to iResolution
     uniform float u_time; // Equivalent to iTime
-    
-    #define FALLING_SPEED  0.01
-    #define STRIPES_FACTOR 1.0
-    
-    vec3 spectral_colour(float l) // RGB <0,1> <- lambda l <400,700> [nm]
-    {
-        float r=0.0,g=0.0,b=0.0;
-            if ((l>=400.0)&&(l<410.0)) { float t=(l-400.0)/(410.0-400.0); r=    +(0.33*t)-(0.20*t*t); }
-        else if ((l>=410.0)&&(l<475.0)) { float t=(l-410.0)/(475.0-410.0); r=0.14         -(0.13*t*t); }
-        else if ((l>=545.0)&&(l<595.0)) { float t=(l-545.0)/(595.0-545.0); r=    +(1.98*t)-(     t*t); }
-        else if ((l>=595.0)&&(l<650.0)) { float t=(l-595.0)/(650.0-595.0); r=0.98+(0.06*t)-(0.40*t*t); }
-        else if ((l>=650.0)&&(l<700.0)) { float t=(l-650.0)/(700.0-650.0); r=0.65-(0.84*t)+(0.20*t*t); }
-            if ((l>=415.0)&&(l<475.0)) { float t=(l-415.0)/(475.0-415.0); g=             +(0.80*t*t); }
-        else if ((l>=475.0)&&(l<590.0)) { float t=(l-475.0)/(590.0-475.0); g=0.8 +(0.76*t)-(0.80*t*t); }
-        else if ((l>=585.0)&&(l<639.0)) { float t=(l-585.0)/(639.0-585.0); g=0.82-(0.80*t)           ; }
-            if ((l>=400.0)&&(l<475.0)) { float t=(l-400.0)/(475.0-400.0); b=    +(2.20*t)-(1.50*t*t); }
-        else if ((l>=475.0)&&(l<560.0)) { float t=(l-475.0)/(560.0-475.0); b=0.7 -(     t)+(0.30*t*t); }
 
-        return vec3(r,g,b);
-    }
     
     void main() {
         vec2 fragCoord = gl_FragCoord.xy;
         // Normalize pixel coordinates
-        vec2 uv = fragCoord / u_resolution;
-
-        vec2 p=(2.0*fragCoord.xy-u_resolution.xy)/min(u_resolution.x,u_resolution.y);
-
-        p*=2.0;
-
-        for(int i=0;i<8;i++)
-        {
-            vec2 newp=vec2(
-                p.y+cos(p.x+u_time)-sin(p.y*cos(u_time*0.2)),
-                p.x-sin(p.y-u_time)-cos(p.x*sin(u_time*0.3))
-            );
-            p=newp;
+        vec4 O = vec4(0.0);
+    
+        // Loop 49 times
+        for (float i = 1.0; i < 50.0; i++) {
+            // Pick layer color
+            O += (i * cos(i + vec4(0, 2, 4, 0)) + i) / 1e4 / 
+                 // Attenuate from orbs
+                 length(fract(fragCoord / u_resolution.y * 20.0 / i + u_time * 0.2 + cos(i * vec2(9, 7))) - 0.5);
         }
-
-        gl_FragColor=vec4(spectral_colour(p.y*50.0+500.0+sin((u_time * 0.1)*0.6)),1.0);
+        
+        // Square for better gamma
+        O *= O;
+        gl_FragColor = O;
     }
     `;
 
