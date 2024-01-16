257bffb141c8eab93181bf81c70a18ce8455a62f
Jake Herbel <iamjake@MacBook-Pro.local>
2024-01-16 00:10:47 -0800

    fixed animations, added a favicon

--- src/components/Hero/Hero.react.js
+++ src/components/Hero/Hero.react.js
@@ -4,8 +4,11 @@ import './Hero.css';
 function Hero() {
   return (
     <div className="hero">
+        
       <h1 className="hero-title">Jake Lately</h1>
-      <p className="hero-subtitle">Software Engineer | Quantum Physics Enthusiast | Music Creator</p>
+      <div>
+            <p className="hero-subtitle">Software Engineer | Quantum Physics Enthusiast | Music Creator</p>
+      </div>
       {/* Add more content or a call-to-action button if needed */}
     </div>
   );
