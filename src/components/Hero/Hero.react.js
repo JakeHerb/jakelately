43cf628009b90e5d819c9a202322fe50f0deb992
Jake Herbel <iamjake@MacBook-Pro.local>
2024-01-16 00:54:20 -0800

    simple changes

--- src/components/Hero/Hero.react.js
+++ src/components/Hero/Hero.react.js
@@ -4,11 +4,10 @@ import './Hero.css';
 function Hero() {
   return (
     <div className="hero">
-        
       <h1 className="hero-title">Jake Lately</h1>
-      <div>
-            <p className="hero-subtitle">Software Engineer | Quantum Physics Enthusiast | Music Creator</p>
-      </div>
+        <div>
+            <p className="hero-subtitle">Software Engineer | Future Enthusiast | Music Creator</p>
+        </div>
       {/* Add more content or a call-to-action button if needed */}
     </div>
   );
