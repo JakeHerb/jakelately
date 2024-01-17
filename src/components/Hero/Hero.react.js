186e6e7f44e7d447cceaa9f1cf800ec3192d2a6f
Jake Herbel <iamjake@MacBook-Pro.local>
2024-01-16 21:18:23 -0800

    Added background images from Canva

--- src/components/Hero/Hero.react.js
+++ src/components/Hero/Hero.react.js
@@ -6,12 +6,15 @@ function Hero() {
     const heroStyle = {
         height: '70dvh',
     }
-
+    const lineStyle = {
+        backgroundColor: '#f4ddb5', // Set the color of the line
+      };
   return (
     <div className="hero" style={heroStyle}>
-      <h1 className="hero-title">Jake Lately</h1>
+      <h1 className="hero-title">Jake <span className="turqiose">Lately</span></h1>
+      <div className="horizontal-line" style={lineStyle}></div> {/* Horizontal line */}
         <div>
-            <p className="hero-subtitle">Software Engineer | Future Enthusiast | Music Creator</p>
+            <p className="hero-subtitle">How can <span className="yellow">Jake</span> help <span className="turqiose">you</span>?&nbsp;</p>
         </div>
       {/* Add more content or a call-to-action button if needed */}
     </div>
