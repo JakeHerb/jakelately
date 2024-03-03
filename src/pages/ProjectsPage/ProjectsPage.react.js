99bf2d95f93dde1b8ac272902e90ca15af65b575
Jake Herbel <iamjake@MacBook-Pro.local>
2024-03-03 00:22:03 -0800

    Spinny space man

--- src/pages/ProjectsPage/ProjectsPage.react.js
+++ src/pages/ProjectsPage/ProjectsPage.react.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProjectCard from './ProjectCard/ProjectCard.react'; // Adjust path as needed
 import './ProjectsPage.css'; // Styling for the Projects page
 import marsRoverPic from './marsRover.jpg';
@@ -25,12 +25,40 @@ const projectData = [
 
 function ProjectsPage() {
 
+  const [isSpinning, setIsSpinning] = useState(false);
+
+  const handleSpinToggle = () => {
+    setIsSpinning(true);
+
+    // Set a timeout to reset isSpinning back to false after 2 seconds
+    const timeoutId = setTimeout(() => {
+      setIsSpinning(false);
+    }, 2000); // 2000 milliseconds = 2 seconds
+
+    // Cleanup timeout when component unmounts or before re-running the effect
+    return () => clearTimeout(timeoutId);
+  };
+
+  // useEffect to handle cleanup
+  useEffect(() => {
+    let timeoutId;
+    if (isSpinning) {
+      timeoutId = setTimeout(() => {
+        setIsSpinning(false);
+      }, 5000);
+    }
+
+    return () => clearTimeout(timeoutId);
+  }, [isSpinning]);
+
   return (
     <div className="projectsBackground" style={{ backgroundImage: `url(${projectBackground})` }}>
       <div className="projects-title">
         <h1>PROJECTS</h1>
       </div>
-      <img src={astronautImage} alt="Floating Astronaut" className="dunking-astronaut"/>
+      <div className={`animated-background ${isSpinning ? 'spin-animation' : 'float-animation'}`} onClick={handleSpinToggle}>
+        <img src={astronautImage} alt="Floating Astronaut" className="dunking-astronaut"/>
+      </div>
       <div className="projects-wrapper">
         <div className="projects-container">
           {projectData.map((project) => (
