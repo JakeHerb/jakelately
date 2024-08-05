4012e324457228c7bb45a2ea2cf861700ae40c79
Jake Herbel <iamjake@MacBook-Pro.local>
2024-08-05 13:50:36 -0700

    Add a bunny and make stuff spin

--- src/pages/ProjectsPage/ProjectCard/ThreeJSProject/ThreeJSProject.react.js
+++ src/pages/ProjectsPage/ProjectCard/ThreeJSProject/ThreeJSProject.react.js
@@ -1,17 +1,26 @@
 import React, { useRef, useState } from 'react'
+import * as THREE from 'three'
 import { Canvas, useFrame } from '@react-three/fiber'
 import './ThreeJSProject.css';
+
+import { useGLTF, Center, Effects, MeshTransmissionMaterial } from '@react-three/drei'
 
 
 function ThreeJSProject() {
     return (
-      <Canvas>
-      <ambientLight intensity={Math.PI / 2} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      <div style={{width: '100%', height: 500, alignContent:"center"}}>
+      <Canvas >
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+        <Center top rotation={[0, -Math.PI / 1.5, 0]} position={[0, 0, 3]}>
+          <Model scale={0.8} />
+        </Center>
+        <Rig />
     </Canvas>
+    </div>
       )
     }
 
@@ -33,10 +42,27 @@ function ThreeJSProject() {
           onClick={(event) => setActive(!active)}
           onPointerOver={(event) => setHover(true)}
           onPointerOut={(event) => setHover(false)}>
-          <boxGeometry args={[1, 1, 1]} />
+          <boxGeometry args={[2, 2, 2]} />
           <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
         </mesh>
       )
+    }
+
+    function Model(props) {
+      const { nodes } = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/bunny/model.gltf')
+      return (
+        <mesh castShadow receiveShadow geometry={nodes.bunny.geometry} {...props}>
+          <MeshTransmissionMaterial backside thickness={0.2} anisotropicBlur={0.1} chromaticAberration={0.1} clearcoat={1} />
+        </mesh>
+      )
+    }
+
+    
+    function Rig({ vec = new THREE.Vector3() }) {
+      useFrame((state) => {
+        state.camera.position.lerp(vec.set(10 * state.pointer.x, 0.5, 3), 0.01)
+        state.camera.lookAt(0, 0, 0)
+      })
     }
 
 export default ThreeJSProject;
